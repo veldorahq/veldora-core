a6e56c55fe49c12471ecd5f774020f813cd5c9fc
Veldora <modraostudio@gmail.com>
2026-08-30 05:33:52 +0600

    release: v0.5.4 sync framework core and .gitattributes

--- src/Foundation/Application.php
+++ src/Foundation/Application.php
@@ -13,7 +13,7 @@ use Veldora\Framework\Session\FileDriver;
 
 final class Application extends Container
 {
-    public const VERSION = '0.5.3';
+    public const VERSION = '0.5.4';
 
     /**
      * The static instance of the application.
